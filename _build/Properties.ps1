a15a1ca4fc64daad408cfaa84dfa94ec4fe95fd8
Raoul Ellias <raoul@mythical.games>
2021-11-03 11:18:51 -0500

    GSC-199 nuget publish

--- _build/Properties.ps1
+++ _build/Properties.ps1
@@ -12,18 +12,18 @@ properties {
         $global:project_configuration = "Release"
     }
 
-    $build_artifacts_dir = "$build_working_dir\BuildArtifacts"
-    $test_results_dir = "$build_artifacts_dir\TestResults"
-    $code_coverage_dir = "$build_artifacts_dir\CodeCoverageReport"
-    $packages_root_dir = "$build_artifacts_dir\packages"
+    $build_artifacts_dir = "$build_working_dir/BuildArtifacts"
+    $test_results_dir = "$build_artifacts_dir/TestResults"
+    $code_coverage_dir = "$build_artifacts_dir/CodeCoverageReport"
+    $packages_root_dir = "$build_artifacts_dir/packages"
 
-    $solution_dir = "$build_working_dir\src"
-    $solution_file = "$solution_dir\IviSdk.sln"
+    $solution_dir = "$build_working_dir/src"
+    $solution_file = "$solution_dir/IviSdk.sln"
 
-    $sdk_csproj_path = "$solution_dir\IviSdk\IviSdkCsharp.csproj"
-    $sdk_output_path = "$solution_dir\IviSdk\bin\$($global:project_configuration)\net5.0"
+    $sdk_csproj_path = "$solution_dir/IviSdk/IviSdkCsharp.csproj"
+    $sdk_output_path = "$solution_dir/IviSdk/bin/$($global:project_configuration)/net5.0"
     $sdk_assembly_name = "Mythical.Game.IviSdkCSharp"
-    $sdk_dll_path = "$sdk_output_path\$($sdk_assembly_name).dll"
+    $sdk_dll_path = "$sdk_output_path/$($sdk_assembly_name).dll"
     $sdk_nuget_package_output_dir = $packages_root_dir
      
     $now = Get-Date
@@ -31,12 +31,12 @@ properties {
 
     $global:logSettings = @{
         logToFile   = $true;
-        logFilePath = "$PSScriptRoot\buildlogs\log-$logFileNameSegment.txt";
+        logFilePath = "$PSScriptRoot/buildlogs/log-$logFileNameSegment.txt";
     }
 
 
     if ($on_build_server -eq $false ) {   
-        $global:logSettings.logFilePath = "$PSScriptRoot\buildlogs\log-$logFileNameSegment.txt";
+        $global:logSettings.logFilePath = "$PSScriptRoot/buildlogs/log-$logFileNameSegment.txt";
     }
 
     $logFile_dir = [System.IO.Path]::GetDirectoryName($global:logSettings.logFilePath)
