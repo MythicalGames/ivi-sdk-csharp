3ea3612799586f5f1149ae3d75acfd920dbedc82
Yuriy Lyeshchenko <yuriy@mythical.games>
2021-11-16 16:10:27 -0600

    feat(model-generation): use built-in tools to manage generated code indentation [GSC-217]

--- src/IviSdkCsharp.ModelsGeneration/OutputGenerator.cs
+++ src/IviSdkCsharp.ModelsGeneration/OutputGenerator.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace IviSdkCsharp.ModelsGeneration
 {
     internal static class OutputGenerator
     {
-        private const string Indent = "\t";
-        private const string DoubleIndent = "\t\t";
-        private const string TripleIndent = "\t\t\t";
         private const string MythicalNamespace = "Mythical.Game.IviSdkCSharp.Model";
 
         internal static string GenerateClass(INamedTypeSymbol targetType, HashSet<string> namespaces, string modelName)
@@ -34,31 +32,31 @@ namespace IviSdkCsharp.ModelsGeneration
                 var propertyType = GetPropertyType(isGoogle, isGrpcGenerated, modelProp);
                 var name = modelProp.Name;
                 modelProperties[name] = propertyType;
-                props.Append(DoubleIndent).Append("public ").Append(propertyType.Value).Append(" ").Append(name)
+                props.Append("public ").Append(propertyType.Value).Append(" ").Append(name)
                     .Append(" { get; set; }")
                     .AppendLine();
             }
 
             string constructors = GenerateConsructors(modelName, modelProperties);
 
-            return $@"{string.Join(Environment.NewLine, namespaces)}
+            return Beautify($@"{string.Join(Environment.NewLine, namespaces)}
 
 namespace {MythicalNamespace}
 {{    
-{Indent}public partial class {modelName}
-{Indent}{{
+public partial class {modelName}
+{{
 {constructors}
 {props}
-{Indent}}}
 }}
-";
+}}
+");
         }
 
         private static string GenerateConsructors(string modelName, Dictionary<string, PropertyType> modelProperties)
         {
-            StringBuilder result = new(DoubleIndent);
+            StringBuilder result = new();
             result.Append("public ").Append(modelName).Append("()").AppendLine();
-            result.Append(DoubleIndent).Append("{");
+            result.Append("{");
             foreach (var prop in modelProperties.Where(x => x.Value.ForPropertyType.IsReferenceType))
             {
                 if (prop.Value.ForPropertyType.SpecialType == SpecialType.System_String)
@@ -70,13 +68,13 @@ namespace {MythicalNamespace}
                     AddPropertyInitialize(prop.Key, $"new {prop.Value.Value}()");
                 }
             }
-            result.AppendLine().Append(DoubleIndent).Append("}");
+            result.AppendLine().Append("}");
             return result.ToString();
 
             void AddPropertyInitialize(string name, string initValue)
             {
                 result.AppendLine();
-                result.Append(TripleIndent).Append(name);
+                result.Append(name);
                 result.Append(" = ");
                 result.Append(initValue).Append(";");
             }
@@ -87,13 +85,13 @@ namespace {MythicalNamespace}
             var enumSyntax = targetType.DeclaringSyntaxReferences[0].GetSyntax();
             var enumDefinition = Regex.Replace(enumSyntax.ToString(), @"\[.+]\s+", "", RegexOptions.Multiline)
                 .Replace(targetType.Name, modelName);
-            return $@"{string.Join(Environment.NewLine, namespaces)}
+            return Beautify($@"{string.Join(Environment.NewLine, namespaces)}
 
 namespace {MythicalNamespace}
 {{    
 {enumDefinition}
 }}
-";
+");
         }
 
         private static PropertyType GetPropertyType(bool isGoogle, bool isGrpcGenerated, IPropertySymbol modelProp)
@@ -136,6 +134,8 @@ namespace {MythicalNamespace}
             SpecialType.System_Boolean => "bool",
             _ => type.Name
         };
+
+        private static string Beautify(string code) => SyntaxFactory.ParseSyntaxTree(code).GetRoot().NormalizeWhitespace()?.ToString() ?? code;
 
         private class PropertyType
         {
