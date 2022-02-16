8d2b496ced190e15199b881c4e8221db04a9130e
Peter Rausch <peter@mythical.games>
2022-02-16 12:12:24 -0600

    fix(uphold): Incorrect version number [GSC-213]

--- SharedConfig/CommonAssemblyInfo.cs
+++ SharedConfig/CommonAssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.0.11")]
-[assembly: AssemblyFileVersion("0.0.0.11")]
+[assembly: AssemblyVersion("0.0.0.14")]
+[assembly: AssemblyFileVersion("0.0.0.14")]
