5a283391b226791d8ca70584d2b4de70882d8a12
Peter Rausch <peter@mythical.games>
2022-02-18 16:28:31 -0600

    fix: Broken build script made version 15 wrong [GSC-213]

--- SharedConfig/CommonAssemblyInfo.cs
+++ SharedConfig/CommonAssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.0.15")]
-[assembly: AssemblyFileVersion("0.0.0.15")]
+[assembly: AssemblyVersion("0.0.0.16")]
+[assembly: AssemblyFileVersion("0.0.0.16")]
