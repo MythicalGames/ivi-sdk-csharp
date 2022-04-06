764f0b8c1b21d9f6b57734ab5240c6196ea9bd0c
Yuriy Lyeshchenko <yuriy@mythical.games>
2022-04-06 17:21:34 -0500

    feat(player): add more logs - up version [GSC-570]

--- SharedConfig/CommonAssemblyInfo.cs
+++ SharedConfig/CommonAssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.0.16")]
-[assembly: AssemblyFileVersion("0.0.0.16")]
+[assembly: AssemblyVersion("0.9.2.3")]
+[assembly: AssemblyFileVersion("0.9.2.3")]
